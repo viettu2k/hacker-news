e2841ec64609cb09db6f15ba9de8782bf31a730c
viettu2k <doanviettu2k@gmail.com>
2021-08-24 08:52:13 +0700

    seventh advanced project react tutorial

---
+++ src/Buttons.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const Buttons = () => {
+    const { isLoading, page, nbPages, handlePage } = useGlobalContext();
+
+    return (
+        <div className="btn-container">
+            <button
+                disabled={isLoading}
+                className="btn"
+                onClick={() => handlePage("dec")}
+            >
+                prev
+            </button>
+            <p>
+                {page + 1} of {nbPages}
+            </p>
+            <button
+                disabled={isLoading}
+                className="btn"
+                onClick={() => handlePage("inc")}
+            >
+                next
+            </button>
+        </div>
+    );
+};
+
+export default Buttons;
